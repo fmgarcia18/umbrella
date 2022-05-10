4744a78bcbc1c865a0050c3c5b9f2c9b3d6cb69a
Francys Garcia <francys@poincenot.com>
2022-05-10 16:26:38 -0300

    Initial commit

---
+++ src/test/java/com/fmgarcia/umbrella/UmbrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmgarcia.umbrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
